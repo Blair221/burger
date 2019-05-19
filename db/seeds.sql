fe7d45d8cc1843987c0eab9805b26d27d21aed26
Blair Brownlie <bbrownli@asu.edu>
2019-05-19 15:36:49 -0700

    adding schema and seeds files, as well as configuring the connection to the MySQL database

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+insert into burgers (burger_name, devoured)
+values ('big kahuna burger', false);
+
+insert into burgers (burger_name, devoured)
+values ('Krabby Patty', false);
+
+insert into burgers (burger_name, devoured)
+values ('The good burger', false);
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+let PORT = process.env.PORT || 3000;
+
+let app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, function (){
+    console.log(`successfully connected on port ${PORT}`);
+})
