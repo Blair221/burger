7b4ee4bb607b8752cd0e1f43b86e82bc1fceb5ad
Blair Brownlie <bbrownli@asu.edu>
2019-05-22 00:10:46 -0700

    trying to set up my jaws_db connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,24 @@
 const mysql = require("mysql");
+var connection;
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  password: "Stev!eg33",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    port: 3306,
+    password: "Stev!eg33",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(error) {
-    if (error){
-        console.log(error.stack);
-        return;
-    }
-    console.log(`successfully connected with id ${connection.threadId}`);
+  if (error) {
+    console.log(error.stack);
+    return;
+  }
+  console.log(`successfully connected with id ${connection.threadId}`);
 });
 
 module.exports = connection;
